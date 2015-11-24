7ed21f20dfd94cb3c546843d171222a63446926c
Eberhard Beilharz <eb1@sil.org>
2015-11-24 18:10:18 +0100

    Fix libgdiplus to handle 2bpp color PNG files
    This fixes https://bugzilla.xamarin.com/show_bug.cgi?id=27616 and
    BL-970.
    
    Change-Id: Ic3d766cf5f284bfdcdf0ac1d906b51376d820dd1
    
    Conflicts:
    	src/pngcodec.c

--- src/pngcodec.c
+++ src/pngcodec.c
@@ -122,7 +122,7 @@ gdip_load_png_properties (png_structp png_ptr, png_infop info_ptr, png_infop end
 		png_uint_32	res_y;
 		png_get_pHYs( png_ptr, info_ptr, &res_x, &res_y, &unit_type );
 		if (unit_type == PNG_RESOLUTION_METER) {
-			bitmap_data->image_flags |= ImageFlagsHasRealDPI;
+		bitmap_data->image_flags |= ImageFlagsHasRealDPI;
 			bitmap_data->dpi_horz = res_x * 0.0254;
 			bitmap_data->dpi_vert = res_y * 0.0254;
 		}
@@ -352,28 +352,28 @@ gdip_load_png_image_from_file_or_stream (FILE *fp, GetBytesDelegate getBytesFunc
 			int num_trans;
 			png_color_16p trans_color;
 
-			/* Make sure transparency is respected. */
+		/* Make sure transparency is respected. */
 			png_get_tRNS( png_ptr, info_ptr, &trans_alpha, &num_trans, &trans_color );
 			if (num_trans > 0) {
-				palette->Flags |= PaletteFlagsHasAlpha;
-				colourspace_flag |= ImageFlagsHasAlpha;
+			palette->Flags |= PaletteFlagsHasAlpha;
+			colourspace_flag |= ImageFlagsHasAlpha;
 
 				if (num_trans > num_palette) {
 					num_trans = num_palette;
-				}
+			}
 
 				for (i=0; i < num_trans; i++) {
-					set_pixel_bgra(&palette->Entries[i], 0,
+				set_pixel_bgra(&palette->Entries[i], 0,
 							png_palette[i].blue,
 							png_palette[i].green,
 							png_palette[i].red,
 #if PNG_LIBPNG_VER > 10399
 							trans_alpha [i]); /* alpha */
 #else
-							info_ptr->trans[i]); /* alpha */
+						info_ptr->trans[i]); /* alpha */
 #endif
-				}
 			}
+		}
 		}
 
 		result = gdip_bitmap_new_with_frame (&gdip_image_frameDimension_page_guid, TRUE);
@@ -417,6 +417,7 @@ gdip_load_png_image_from_file_or_stream (FILE *fp, GetBytesDelegate getBytesFunc
 		png_bytep *row_pointers;
 		BYTE *rawptr;
 		int i, j;
+		BYTE	alpha[4];	/* transparency values for 2bpp */
 
 		width = png_get_image_width (png_ptr, info_ptr);
 		height = png_get_image_height (png_ptr, info_ptr);
@@ -431,6 +432,7 @@ gdip_load_png_image_from_file_or_stream (FILE *fp, GetBytesDelegate getBytesFunc
 
 		interlace = png_get_interlace_type (png_ptr, info_ptr);
 
+		/* NB: none of these png_set_* methods actually do anything after png_read_png() is called! */
 		/* According to the libpng manual, this sequence is equivalent to
 		* using the PNG_TRANSFORM_EXPAND flag in png_read_png. */
 		if (color_type == PNG_COLOR_TYPE_PALETTE) {
@@ -492,11 +494,24 @@ gdip_load_png_image_from_file_or_stream (FILE *fp, GetBytesDelegate getBytesFunc
 			}
 
 			case 1:
+				if (bit_depth == 2) {
+					/* Make sure transparency is respected for 2bpp images. */
+					memset(alpha, 0xFF, 4);	/* default to fully opaque */
+					int num_trans = 0;
+					BYTE * trans = NULL;
+					png_color_16p dummy;
+					if (png_get_tRNS(png_ptr, info_ptr, &trans, &num_trans, &dummy)) {
+						if (num_trans > 0 && trans != NULL) {
+							memcpy(alpha, trans, MIN(num_trans, 4));
+						}
+					}
+				}
 				for (i = 0; i < height; i++) {
 					png_bytep rowp = row_pointers[i];
+					rawptr = rawdata + i * stride;	/* Ensure each output row starts at the right place. */
 					if (bit_depth == 2) {
 						// 4 pixels for each byte
-						for (j = 0; j < (width >> bit_depth); j++) {
+						for (j = 0; j < width; j++) {
 							png_byte palette = 0;
 							png_byte pix = *rowp++;
 
@@ -505,28 +520,34 @@ gdip_load_png_image_from_file_or_stream (FILE *fp, GetBytesDelegate getBytesFunc
 								png_palette[palette].blue,
 								png_palette[palette].green,
 								png_palette[palette].red,
-								0xFF); /* alpha */
+								alpha[palette]);
+							if (++j >= width)
+								break;
 
 							palette = (pix >> 4) & 0x03;
 							set_pixel_bgra (rawptr, 4,
 								png_palette[palette].blue,
 								png_palette[palette].green,
 								png_palette[palette].red,
-								0xFF); /* alpha */
+								alpha[palette]);
+							if (++j >= width)
+								break;
 
 							palette = (pix >> 2) & 0x03;
 							set_pixel_bgra (rawptr, 8,
 								png_palette[palette].blue,
 								png_palette[palette].green,
 								png_palette[palette].red,
-								0xFF); /* alpha */
+								alpha[palette]);
+							if (++j >= width)
+								break;
 
 							palette = pix & 0x03;
 							set_pixel_bgra (rawptr, 12,
 								png_palette[palette].blue,
 								png_palette[palette].green,
 								png_palette[palette].red,
-								0xFF); /* alpha */
+								alpha[palette]);
 							rawptr += 16;
 						}
 					} else {
@@ -567,6 +588,10 @@ gdip_load_png_image_from_file_or_stream (FILE *fp, GetBytesDelegate getBytesFunc
 			// doesn't apply to 2bpp images
 			result->active_bitmap->pixel_format = PixelFormat8bppIndexed;
 			result->active_bitmap->image_flags = ImageFlagsColorSpaceGRAY;
+		} else if ((channels == 1) && (color_type == PNG_COLOR_TYPE_PALETTE)) {
+			// does apply to (what were) 2bpp images
+			result->active_bitmap->image_flags = ImageFlagsColorSpaceRGB;
+			result->active_bitmap->image_flags |= ImageFlagsHasAlpha;
 		}
 
 		if (color_type & PNG_COLOR_MASK_ALPHA)
